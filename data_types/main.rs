4ed28b4d7077c5204c67bb0a994e26a6761577e7
sankarebarri <sankarebarri_dev@yahoo.com>
2025-09-09 09:46:12 +0000

    rust_project

---
+++ data_types/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let x = 5 + 3;
+    let y = 10.5 - 6.0;
+    let z = 4 * 30;
+
+    let a = 56.7 / 32.2;
+    let b = -5 / 3;
+    let c = 56.0 / 32.0;
+    let d = 43 % 5;
+
+    let t = true;
+    let f: bool = false;
+
+    let e = 'z';
+    let g: char = 'b';
+    let heart_eyed_cat = '😻';
+
+    println!("x={x}, y={y}, z={z}, a={a}, b={b}, c={c}, d={d}. {t}, {f}. {e}, {g}, {heart_eyed_cat}");
+}
