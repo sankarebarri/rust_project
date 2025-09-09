83e81950095b51db90af5bc88e2fefec0c9d6dd0
sankarebarri <sankarebarri_dev@yahoo.com>
2025-09-09 11:20:52 +0000

    rust_project

--- data_types/main.rs
+++ data_types/main.rs
@@ -16,4 +16,29 @@ fn main() {
     let heart_eyed_cat = '😻';
 
     println!("x={x}, y={y}, z={z}, a={a}, b={b}, c={c}, d={d}. {t}, {f}. {e}, {g}, {heart_eyed_cat}");
+
+    // Compound types
+    // THe tuple types
+    let tup: (u32, f64, i8) = (65, 20.5, 6);
+    let (x, y, z) = tup;
+    println!("x={x}, y={y}, z={z}");
+
+    // Accessing tuple elements with .
+    let sixty_five = tup.0;
+    let twenty_point_five = tup.1;
+    let six = tup.2;
+    println!("sixty_five={sixty_five}, twenty_point_five={twenty_point_five}, six={six}");
+
+    // Arrays
+    let arr: [i32; 6] = [1, 2, 3, 4, 5, 6];
+    let first = arr[0];
+    println!("first={first}");
+    let arr = [1, 2, 3, 4, 5, 6];
+    let last = arr[5];
+    println!("last={last}");
+
+    // initialising the same element
+    let arr = [3; 5]; // [3, 3, 3, 3, 5]
+    let second = arr[1];
+    println!("second={second}");
 }
